4297f4168c069c8c303c60cf90fa7104cb3e24ec
ajay26188 <ajaysah52@gmail.com>
2025-07-24 10:57:26 +0530

    setting up server with TS

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+require ('dotenv').config()
+import express from 'express'
+const app = express()
+
+app.get('/ping', (_req, res) => {
+    res.send('pong')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
